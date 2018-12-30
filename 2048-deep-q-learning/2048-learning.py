57c3e5eb33d1a89efe6610605e7eb1e63dee73ce
Jack Shea <jshea@Jacks-MacBook-Pro-3.local>
2018-12-30 15:40:58 -0500

    updates

--- 2048-deep-q-learning/2048-learning.py
+++ 2048-deep-q-learning/2048-learning.py
@@ -27,16 +27,14 @@ class DQNetwork:
         self.fc = tf.layers.dense(
             inputs = self.state_input,
             units = 256,
-            activation = tf.nn.elu,
             kernel_initializer = tf.contrib.layers.xavier_initializer()
         )
 
         # Defines output layer
         self.output = tf.layers.dense(
             inputs = self.fc,
-            kernel_initializer=tf.contrib.layers.xavier_initializer(),
             units = 4,
-            activation=None
+            kernel_initializer=tf.contrib.layers.xavier_initializer()
         )
 
         # Q is our predicted Q value.
@@ -46,23 +44,6 @@ class DQNetwork:
         # Sum(Qtarget - Q)^2
         self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
         self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
-
-
-class Memory():
-    def __init__(self, max_size):
-        self.buffer = deque(maxlen = max_size)
-    
-    def add(self, experience):
-        self.buffer.append(experience)
-    
-    def sample(self, batch_size):
-        buffer_size = len(self.buffer)
-        index = np.random.choice(
-            np.arange(buffer_size),
-            size = batch_size,
-            replace = False
-        )
-        return [self.buffer[i] for i in index]
 
 
 class Game:
@@ -82,7 +63,7 @@ class Game:
 
     def update_board(self, action):
 
-        action = [up,down,left,right][action.tolist().index(1)]
+        action = [up,down,left,right][action]
         self.matrix, done, d_score = action(self.matrix)
         game_over = False
         if done:
@@ -105,60 +86,21 @@ class Game:
         return str(self.matrix[0]) + "\n" + str(self.matrix[1]) + "\n" + str(self.matrix[2]) + "\n" + str(self.matrix[3])
 
 
-
-def populate_memory(game, memory):
-
-    for i in range(pretrain_length):
-        
-        # If it's the first step, flatten the board matrix and set state variable
-        if i == 0:
-            state = np.asarray([value for row in game.matrix for value in row])
-        
-        # Choose a random action and evaluate reward
-        action = np.asarray(random.choice(possible_actions))
-        reward, game_over = game.update_board(action)
-        
-        # If we're dead
-        if game_over:
-            # We finished the episode
-            next_state = np.zeros(state.shape)
-            
-            # Add experience to memory
-            memory.add((state, action, reward, next_state, game_over))
-            
-            # Start a new episode
-            game.init_matrix()
-            
-            # First we need a state
-            state = np.asarray([value for row in game.matrix for value in row])
-            
-        else:
-            # Get the next state
-            next_state = np.asarray([value for row in game.matrix for value in row])
-            
-            # Add experience to memory
-            memory.add((state, action, reward, next_state, game_over))
-            
-            # Our state is now the next_state
-            state = next_state
-    
-    return memory
-
-def train(game, memory):
+def train(game):
 
     with tf.Session() as sess:
 
         # Initialize the weights of the network
         sess.run(tf.global_variables_initializer())
         
-        # Initialize the decay rate (that will use to reduce epsilon) 
+        # Initialize the decay rate (that will use to reduce epsilon)
         decay_step = 0
 
         # Trains the network
         for episode in range(total_episodes):
 
             # Resets the step count
-            step = 0       
+            step = 0
             
             # List to track the rewards gained throughout the episode
             episode_rewards = []
@@ -171,21 +113,19 @@ def train(game, memory):
                 step += 1
                 
                 # Increase decay_step
-                decay_step +=1
+                decay_step += 1
                 
-                # Predict the action to take and take it
+                # Decide next action to take
                 action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, sess)
 
-                # Do the action
+                # Perform action and get reward
                 reward, game_over = game.update_board(action)
                 
                 # Add the reward to total reward
                 episode_rewards.append(reward)
 
-                # If the game is finished
+                # If the game is finished, end the episode and aggregate rewards
                 if game_over:
-                    # the episode ends so no next state
-                    next_state = np.zeros(state.shape)
 
                     # Set step = max_steps to end the episode
                     step = max_steps
@@ -193,58 +133,23 @@ def train(game, memory):
                     # Get the total reward of the episode
                     total_reward = np.sum(episode_rewards)
 
-                    print('Episode: {}'.format(episode),
-                            'Total reward: {}'.format(total_reward),
-                            'Training loss: {:.4f}'.format(loss),
-                            'Explore P: {:.4f}'.format(explore_probability))
+                # Get the next state value
+                next_state = np.asarray([value for row in game.matrix for value in row])
 
-                    memory.add((state, action, reward, next_state, game_over))
+                # Gets all the Q values for actions from the next state
+                Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.state_input: [next_state]})
 
-                else:
-                    # Get the next state
-                    next_state = np.asarray([value for row in game.matrix for value in row])
-                    
-                    # Add experience to memory
-                    memory.add((state, action, reward, next_state, game_over))
-                    
-                    # st+1 is now our current state
-                    state = next_state
+                # Target value is reward gained + all expected future rewards
+                target = reward + gamma * np.max(Qs_next_state)
 
-
-                ### LEARNING PART            
-                # Obtain random mini-batch from memory
-                batch = memory.sample(batch_size)
-                states_mb = np.array([each[0] for each in batch], ndmin=2)
-                actions_mb = np.array([each[1] for each in batch])
-                rewards_mb = np.array([each[2] for each in batch])
-                next_states_mb = np.array([each[3] for each in batch], ndmin=2)
-                dones_mb = np.array([each[4] for each in batch])
-
-                target_Qs_batch = []
-
-                # Get Q values for next_state 
-                Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.state_input: next_states_mb})
-                
-                # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
-                for i in range(0, len(batch)):
-                    terminal = dones_mb[i]
-
-                    # If we are in a terminal state, only equals reward
-                    if terminal:
-                        target_Qs_batch.append(rewards_mb[i])
-                        
-                    else:
-                        target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
-                        target_Qs_batch.append(target)
-                        
-
-                targets_mb = np.array([each for each in target_Qs_batch])
+                action_vect = [0, 0, 0, 0]
+                action_vect[action] = 1
                 loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
-                                    feed_dict={DQNetwork.state_input: states_mb,
-                                            DQNetwork.target_Q: targets_mb,
-                                            DQNetwork.action_output: actions_mb})
+                                    feed_dict={DQNetwork.state_input: [state],
+                                            DQNetwork.target_Q: [target],
+                                            DQNetwork.action_output: [action_vect]})
 
-
+            print("Episode complete, loss: {}".format(loss))
 
 
 
@@ -259,18 +164,16 @@ def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, s
     
     if (explore_probability > exp_exp_tradeoff):
         # Make a random action (exploration)
-        action = random.choice([0,1,2,3])
-        action = np.asarray([0]*action + [1] + [0]*(3-action))
+        action = random.randint(0,3)
         
     else:
         # Get action from Q-network (exploitation)
         # Estimate the Qs values state
-        Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.state_input: state.reshape(1,16)})
+        Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.state_input: [state]})
         
         # Take the biggest Q value (= the best action)
         choice = np.argmax(Qs)
         action = int(choice)
-        action = np.asarray([0]*action + [1] + [0]*(3-action))
 
     return action, explore_probability
 
@@ -306,15 +209,9 @@ memory_size = 1000000          # Number of experiences the Memory can keep
 
 # Creates network and memory
 DQNetwork = DQNetwork(state_size, action_size, learning_rate)
-memory = Memory(max_size = memory_size)
 
 # Create the environment
 game = Game()
 
-# Populate the memory buffer and reset game
-memory = populate_memory(game, memory)
-game = Game()
-
 # Conduct training
-train(game, memory)
-
+train(game)
